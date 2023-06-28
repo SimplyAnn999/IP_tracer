4e19c57109310ff3e5d324df172e2846f9204166
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-06-28 02:24:09 -0500

    IPtrace streamlit_app.py

---
+++ IPtrace streamlit_app.py
@@ -0,0 +1,16 @@
+import requests
+
+ip_address = st.text_input("Enter IP address you want to look up : ")
+headers = {
+  "authorization": st.secrets["api_key"]
+}
+
+url = f"http://api.ipstack.com/{ip_address}?access_key={hearders}"
+response = requests.get (url)
+
+if response.status.code == 200:
+  data = response.json()
+  st.write(data)
+
+else:
+  st.write(f"Error {response.status_code}: {response.text}")
